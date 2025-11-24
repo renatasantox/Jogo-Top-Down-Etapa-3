635ddf8d48e917ee04a5add2c109cdd3c6b56b97
renatasantox <renatarafaelapereiradejesis@gmail.com>
2025-11-23 22:40:01 -0300

    23/11 part2

--- Jogo Top Down/Assets/Scripts/Arma.cs
+++ Jogo Top Down/Assets/Scripts/Arma.cs
@@ -1,3 +1,4 @@
+using UnityEditor;
 using UnityEngine;
 
 public class Arma : MonoBehaviour
@@ -18,40 +19,56 @@ public class Arma : MonoBehaviour
 
     void Start()
     {
-        camera = GetComponent<Camera>();
-        spriteRenderer = GetComponent<spriteRenderer>();
+        camera = Camera.main;
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
-        if (gameObject.transform.rotation.eulerAngles.z > -90 && gameObject.tranform.rotation.eulerAngles.z < 90)
+        if (gameObject.transform.rotation.eulerAngles.z > -90 && gameObject.transform.rotation.eulerAngles.z < 90)
         {
-            tranform.local.Scale = new Vector3(1, 1, 1);
+            transform.localScale = new Vector3(1, 1, 1);
         }
 
-        if (gameObject.transform.rotation.eulerAngles.z > 90 && gameObject.tranform.rotation.eulerAngles.z < 270)
+        if (gameObject.transform.rotation.eulerAngles.z > 90 && gameObject.transform.rotation.eulerAngles.z < 270)
         {
-            tranform.local.Scale = new Vector3(1, -1, 1);
+            transform.localScale = new Vector3(1, -1, 1);
         }
 
         // Distância da câmera ao objeto. Precisamos disso para fazer o cálculo correto.
         float camDis = camera.transform.position.y - transform.position.y;
 
         // Obtém a posição do mouse no espaço mundial. Usando camDis para eixo Z.
-        Vector3 mouse = camera.ScreenToWorldPoint(new Vector3(Input.mouse.Position.x, Input.mousePosition.y, camDis));
+        Vector3 mouse = camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, camDis));
 
         float AngleRad = Mathf.Atan2(mouse.y - transform.position.y, mouse.x - transform.position.x);
         float angle = (180 / Mathf.PI) * AngleRad;
 
-        tranform.rotation = Quaternion.AngleAxis(angles, Vector3.forward);
+        transform.rotation = Quaternion.AngleAxis( angle, Vector3.forward);
 
         /// Debug.Log("Angulo: "+angle);
 
-        cursor.tranform.position = new Vector3(mouse.x, mouse.y, cursor.tranform.position.z);
+        cursor.transform.position = new Vector3(mouse.x, mouse.y, cursor.transform.position.z);
 
         Debug.DrawLine(transform.position, mouse, Color.red);
 
 
-        if()
+        if(tempoDeDisparo <= 0 && Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Debug.Log("Bala disparada");
+
+            GameObject b = Instantiate(this.bala, saidaDeTiro.position, saidaDeTiro.rotation);
+
+            tempoDeDisparo = intervaloDeDisparo;
+        }
+
+
+        if(tempoDeDisparo > 0)
+        {
+            tempoDeDisparo -= Time.deltaTime;
+        }
+
+
+
      }
 }
--- Jogo Top Down/Assets/Scripts/Bala.cs
+++ Jogo Top Down/Assets/Scripts/Bala.cs
@@ -2,15 +2,47 @@ using UnityEngine;
 
 public class Bala : MonoBehaviour
 {
+    [SerializeField] private int dano = 1;
+    [SerializeField] private float velocidade = 1.5f;
+
+    private Renderer m_Renderer;
+
+    public void setDano(int dano)
+    {
+        this.dano = dano;
+    }
+
+    public int getDano()
+    {
+        return this.dano;
+    }
 
     void Start()
     {
-        
+        m_Renderer = GetComponent<Renderer>();
     }
 
 
     void Update()
     {
-        
+        transform.Translate(velocidade * Time.deltaTime, 0, 0);
+
+        if(!m_Renderer.isVisible)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D colisao)
+    {
+        if (colisao.gameObject.CompareTag("Inimigo"))
+        {
+            //causa dano ao Inimigo
+            int novaVida = colisao.gameObject.GetComponent<Personagem>().getVida() - getDano();
+            colisao.gameObject.GetComponent<Personagem>().setVida(novaVida);
+        }
+
+        Destroy(this.gameObject);
     }
 }
